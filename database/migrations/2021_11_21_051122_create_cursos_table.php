d3b9c97d3341b7593ce447e27fd81283d103d2f0
jassiel2911 <brian29_11@comunidad.unam.mx>
2021-11-20 23:36:10 -0600

    Modelo, controlador y migración de cursos.

---
+++ database/migrations/2021_11_21_051122_create_cursos_table.php
@@ -0,0 +1,46 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateCursosTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('cursos', function (Blueprint $table) {
+            $table->id();
+
+            $table->string('nombre');
+            $table->date('fecha_inicio');
+            $table->date('fecha_fin');
+            $table->time('hora_inicio');
+            $table->time('hora_fin');
+            $table->string('dias');
+            $table->string('antecedentes');
+            $table->string('equipo');
+            $table->integer('precio_unam');
+            $table->integer('precio_ext');
+            $table->integer('precio_gral');
+            $table->string('temario');
+            $table->string('imagen');
+
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('cursos');
+    }
+}
