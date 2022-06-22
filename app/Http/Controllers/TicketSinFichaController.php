6d0a49b4c2017de7723f3e53555b0a9de893e818
anallely753 <anallely.mtzc@comunidad.unam.mx>
2022-06-22 17:39:23 -0500

    10 fichas de 600

--- app/Http/Controllers/TicketSinFichaController.php
+++ app/Http/Controllers/TicketSinFichaController.php
@@ -168,8 +168,8 @@ class TicketSinFichaController extends Controller
 
         }
         else if($total==1200){
-             $fichas = Ficha::where('monto', 1200)
-             ->where('ticket_id', NULL)->take(1)->get();
+             $fichas = Ficha::where('monto', 600)
+             ->where('ticket_id', NULL)->take(2)->get();
            
             // if($fichas==NULL){
             //      $fichas = Ficha::where('monto', 1200)
